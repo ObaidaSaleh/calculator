32ba1858a758422369a0b72b77d32b66ddbd14dc
ObaidaSaleh <46760451+ObaidaSaleh@users.noreply.github.com>
2020-03-24 09:20:29 -0700

    test change

--- src/CalcManager/CEngine/calc.cpp
+++ src/CalcManager/CEngine/calc.cpp
@@ -1,5 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
+// Licensed under the MIT License. h 
 
 #include <cassert>
 #include "Header Files/CalcEngine.h"
